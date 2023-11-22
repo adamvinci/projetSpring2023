73a72eb0fdf73e2dec38662f8f9d53d81aaef92a
Adame Barhdadi <adame.barhdadi@student.vinci.be>
2023-11-22 15:26:36 +0100

    price proxy

---
+++ execute/src/main/java/be/vinci/ipl/vsx/repositories/PriceProxy.java
@@ -0,0 +1,22 @@
+package be.vinci.ipl.vsx.repositories;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Repository
+@FeignClient(name = "price-service")
+public interface PriceProxy {
+
+    /**
+     * Order service endpoint to update the filled quantity of an order based on its GUID.
+     * @param ticker The GUID of the order to update.
+     * @param newPrice The last price at which the stock has been sold.
+     * @return OK if the price is updated or BAD_REQUEST if the price is not valid
+     */
+    @PutMapping("/price/{ticker}")
+    ResponseEntity<Void> updatePrice(@PathVariable String ticker, @RequestBody Number newPrice);
+}
