107ced69b590ab2f8b3c4c265f00d4dc4601b6a1
bilal el haddadi <bilal.el.haddadi77@gmail.com>
2023-11-29 20:09:11 +0100

    update investor && add javadoc

--- investor/src/main/java/be/vinci/ipl/vsx/investor/InvestorService.java
+++ investor/src/main/java/be/vinci/ipl/vsx/investor/InvestorService.java
@@ -13,6 +13,12 @@ public class InvestorService {
 
     private final InvestorRepository investorRepository;
 
+
+    /**
+     * Constructor for InvestorService.
+     *
+     * @param investorRepo The repository for handling investor data.
+     */
     @Autowired
     public InvestorService(InvestorRepository investorRepo){
         this.investorRepository = investorRepo;
@@ -21,34 +27,50 @@ public class InvestorService {
     public List<Investor> readAll(){
         return (List<Investor>) investorRepository.findAll();
     }
+
+    /**
+     * Retrieve information about a specific investor by username.
+     *
+     * @param username The username of the investor.
+     * @return Investor object containing information about the investor.
+     */
     public Investor getInvestorByUsername(String username) {
         return investorRepository.findById(username).orElse(null);
     }
 
+
+    /**
+     * Create a new investor in the system.
+     *
+     * @param investor The investor data to be created.
+     * @return The created investor, or null if the investor already exists.
+     */
     public Investor createInvestor(Investor investor) {
+        if(investorRepository.existsById(investor.getUsername())){
+            return null;
+        }
         return investorRepository.save(investor);
     }
 
-    public Optional<Investor> updateInvestor(String username, Investor updatedInvestor) {
-        Optional<Investor> existingInvestor = investorRepository.findById(username);
-        if (existingInvestor.isPresent()) {
-            Investor investorToUpdate = existingInvestor.get();
-            investorToUpdate.setEmail(updatedInvestor.getEmail());
-            investorToUpdate.setFirstname(updatedInvestor.getFirstname());
-            investorToUpdate.setLastname(updatedInvestor.getLastname());
-            investorToUpdate.setBirthdate(updatedInvestor.getBirthdate());
-            return Optional.of(investorRepository.save(investorToUpdate));
-        } else {
-            return Optional.empty(); // Investor not found
-        }
-    }
 
+    /**
+     * Update information about a specific investor.
+     *
+     * @param investor The updated investor data.
+     * @return True if the investor is updated, false otherwise.
+     */
     public boolean updateOne(Investor investor) {
         if (!investorRepository.existsById(investor.getUsername())) return false;
         investorRepository.save(investor);
         return true;
     }
 
+
+    /**
+     * Delete a specific investor by username.
+     *
+     * @param username The username of the investor to be deleted.
+     */
     public void deleteInvestor(String username) {
         investorRepository.deleteById(username);
     }
